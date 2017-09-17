de36fe44b31755ba07d0c1b62ca2dd47f2c739f2
arris <akoucheki@yahoo.com>
2017-09-17 17:20:41 -0400

    End Week 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from '@angular/material'; 
-import { FlexLayoutModule } from '@angular/flex-layout';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout'; 
+
+import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material'; 
 
 import { AppComponent } from './app.component';
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule,
-    FlexLayoutModule
+    FormsModule,
+    HttpModule,
+    FlexLayoutModule,
+    MaterialModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
